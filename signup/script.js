eb7b4299bd64b3cc0c72d26b42bd4837f61442b3
sunny8750 <120244832+sunny8750@users.noreply.github.com>
2023-04-05 20:25:31 +0530

    Update script.js

--- signup/script.js
+++ signup/script.js
@@ -46,7 +46,7 @@ var flag = true;
    users.push(user)
    if(flag){
     localStorage.setItem("users", JSON.stringify(users));
-    location.href="./loginpage/"
+    location.href="./loginpage/index.html/"
    }else{
     alert("something went wrong plzz try again")
    } 
